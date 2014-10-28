8892877b47e90ec2cc386050109561cc14a27c75
Dan Bowles <daniel.bowles@gmail.com>
2014-10-27 22:20:11 -0400

    Update 2014-10-28T02:20:03.940Z

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+(function($) {
+  
+  $(function() {
+    $('#nav-link').click(function() {
+      $(this).toggleClass('active');
+      $('ul').toggleClass('active');
+    })
+  });
+})(window.jQuery);
+(function($) {
+  
+  $(function() {
+    $('#nav-link').click(function() {
+      $(this).toggleClass('active');
+      $('ul').toggleClass('active');
+    })
+  });
+})(window.jQuery);
